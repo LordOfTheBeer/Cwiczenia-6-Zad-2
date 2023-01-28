88d01bd17719144e43a3f6c736cda245d728a495
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-28 14:05:39 +0100

    Add files via upload

---
+++ Cwiczenia 6 zad 2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Cwiczenia_6_zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "jakis tam tekst...";
+
+            for (int i= 0; i < tekst.Length; i++)
+
+                Console.Write(tekst[i] + "\n");
+        }
+    }
+}
